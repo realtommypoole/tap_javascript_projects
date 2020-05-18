7142e6ed1053a0d11b9a453cc3f15155be748de3
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-05-18 07:08:45 -0700

    Include second assignemnt
    This update includes the second js assignment.

---
+++ Basic_JavaScript_Projects/js/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function My_First_Function () {         // define the function name
+    var str = "This text is blue.";     // define the first variable, a string
+    var result = str.fontcolor ("blue");// define a method on that first variable, as a variable
+    document.getElementById("Blue_Text").innerHTML = result; //define a method on that second variable, 
+}
+
+function myFunction () {        // define the function name
+    var sentence = "I am learning";     // define the variable the function will be working on.
+    sentence += "a lot from this book!"; // define the second variable of the function
+    document.getElementById("Concatenate").innerHTML = sentence; // define the method that will be called on the two variables and where it will result on the page.
+}
+
