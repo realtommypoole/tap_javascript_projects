5e76b04abd8bc1e0e11f2e9d240fe58eba3a85b8
realtommypoole <65101524+realtommypoole@users.noreply.github.com>
2020-05-17 19:29:57 -0700

    Create first JS file
    This push includes the first JS file.

---
+++ Basic_JavaScript_Projects/js/javascript_file.js
@@ -0,0 +1,10 @@
+var family="Jone\'s", dad = "Bill", mom = "Nancy", son = "Fernando", daughter = "Mia"; //Define string variables for a family
+var A = "Hello World!";      //assigning a string variable
+var B = "This is just a test!"; //assigning another string variable
+var A = A.fontcolor("red"); //changes variable B font color to blue.
+
+window.alert(B); //This method displays var B in a window on the browser
+document.write(A); //This method will display var A on the webpage
+document.write(son + family); //This will concatonate the two variables and write them on the webpage
+document.write(3+3);
+
